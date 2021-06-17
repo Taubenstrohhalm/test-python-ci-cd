1416e38c698d6461f694d1990add28c07ca23fc3
Taubenstrohhalm <>
2021-06-17 18:35:04 +0200

    add: zip code input

--- webapp/apps/location.py
+++ webapp/apps/location.py
@@ -1,14 +1,30 @@
 import dash
 import dash_html_components as html
 import dash_core_components as dcc
+from dash.dependencies import Input, Output, State
+
+from app import app
 
 layout = html.Div(children=[
     html.H1(children='Choose Location'),
 
     html.Div(children='''
-        The location is needed to find information about the climate in your region.
+        Please enter your zip code.
     '''),
+    dcc.Input(id="input", type="text", placeholder="Enter post code", value = ''),
+    html.Button(children = 'Submit', id="submit_button", n_clicks=0),
+    html.Div(id='location_output',children='')
 ])
+
+@app.callback(
+    Output('location_output','children'),
+    Input('submit_button','n_clicks'),
+    State('input','value')
+)
+def show_latlong(n_clicks, value):
+    if n_clicks == 0:
+        return ''
+    return f'Your building is placed at: {value}.'
 
 if __name__ == '__main__':
     app.run_server(debug=True)
