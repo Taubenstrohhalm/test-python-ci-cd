b4ac8f25d7302f020738b05c5e22227ea473d0c0
Taubenstrohhalm <>
2021-06-17 14:44:11 +0200

    edit: entry point

--- index.py
+++ index.py
@@ -2,7 +2,7 @@ import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
 
-from app import app
+from app import app, server
 from apps import _01_landing_page # ,_02_location
 
 
